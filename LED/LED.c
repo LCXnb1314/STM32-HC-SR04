e424672e4fd8e161e97d470436a53e5299cdcf40
LCXnb1314 <79490123+LCXnb1314@users.noreply.github.com>
2021-06-29 18:51:41 +0800

    LED

---
+++ LED/LED.c
@@ -0,0 +1,25 @@
+#include "LED.h"
+#include "stm32f10x.h"                  // Device header
+
+void LED_Init()
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_5;
+	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
+	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE,ENABLE);
+	
+	
+	GPIO_Init(GPIOB,&GPIO_InitStructure);
+	GPIO_SetBits(GPIOB,GPIO_Pin_5);
+	
+	
+	GPIO_Init(GPIOE,&GPIO_InitStructure);
+	GPIO_SetBits(GPIOE,GPIO_Pin_5);
+	
+	
+}
+
+
